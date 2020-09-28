c5e2f40ae6fa7ac230c5e717b7c7385a307e2e81
lzhao2 <lzhao2@paypal.com>
2020-09-28 22:22:58 +0800

    添加第七周作业

---
+++ Week_07/PrefixTrie208.java
@@ -0,0 +1,122 @@
+//实现一个 Trie (前缀树)，包含 insert, search, 和 startsWith 这三个操作。 
+//
+// 示例: 
+//
+// Trie trie = new Trie();
+//
+//trie.insert("apple");
+//trie.search("apple");   // 返回 true
+//trie.search("app");     // 返回 false
+//trie.startsWith("app"); // 返回 true
+//trie.insert("app");   
+//trie.search("app");     // 返回 true 
+//
+// 说明: 
+//
+// 
+// 你可以假设所有的输入都是由小写字母 a-z 构成的。 
+// 保证所有输入均为非空字符串。 
+// 
+// Related Topics 设计 字典树 
+// 👍 418 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class PrefixTrie208 {
+
+    class TrieNode {
+        private TrieNode[] links;
+        private final int R = 26;
+        private boolean isEnd;
+
+        /**
+         * Initialize your data structure here.
+         */
+        public TrieNode() {
+            links = new TrieNode[R];
+        }
+
+        public boolean containKey(char ch) {
+            return links[ch - 'a'] != null;
+        }
+
+        public TrieNode get(char ch) {
+            return links[ch - 'a'];
+        }
+
+        public void put(char ch, TrieNode node) {
+            links[ch - 'a'] = node;
+        }
+
+        public boolean isEnd() {
+            return isEnd;
+        }
+
+        public void setEnd() {
+            isEnd = true;
+        }
+
+    }
+
+    class Trie {
+
+        private TrieNode root;
+
+        public Trie() {
+            root = new TrieNode();
+        }
+
+        /**
+         * Inserts a word into the trie.
+         */
+        public void insert(String word) {
+            TrieNode node = root;
+            for (int i = 0; i < word.length(); i++) {
+                char currentchar = word.charAt(i);
+                if (!node.containKey(currentchar)) {
+                    node.put(currentchar, new TrieNode());
+                }
+                node = node.get(currentchar);
+            }
+            node.setEnd();
+        }
+
+        /**
+         * Returns if the word is in the trie.
+         */
+        public boolean search(String word) {
+            TrieNode node = searchPrefix(word);
+            return node != null && node.isEnd();
+        }
+
+        private TrieNode searchPrefix(String prefix) {
+            TrieNode node = root;
+            for (int i = 0; i < prefix.length(); i++) {
+                char currentchar = prefix.charAt(i);
+                if (node.containKey(currentchar)) {
+                    node = node.get(currentchar);
+                } else {
+                    return null;
+                }
+            }
+            return node;
+        }
+
+        /**
+         * Returns if there is any word in the trie that starts with the given prefix.
+         */
+        public boolean startsWith(String prefix) {
+            TrieNode node = searchPrefix(prefix);
+            return node != null;
+
+        }
+    }
+}
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
